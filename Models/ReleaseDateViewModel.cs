8cbc8ba713d3638da692a853da7f1469ec5d01c5
ashjoy7 <122189249+ashjoy7@users.noreply.github.com>
2023-11-11 12:39:17 -0800

    Add project files.

---
+++ Models/ReleaseDateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models;
+
+public class ReleaseDateViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? DateTime { get; set; }
+    public string? ReleaseDate { get; set; }
+    public string? SearchString { get; set; }
+    public SelectList DateTimeOptions { get; internal set; }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "17 Miracles",
+                    ReleaseDate = DateTime.Parse("2011-3-6"),
+                    Genre = "Drama",
+                    Rating = "PG",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "Forever Strong",
+                    ReleaseDate = DateTime.Parse("2008-26-10"),
+                    Genre = "Drama",
+                    Rating = "PG-13",
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Title = "The Saratov Approach",
+                    ReleaseDate = DateTime.Parse("2013-9-10"),
+                    Genre = "Drama",
+                    Rating = "PG-13",
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Title = "Charly",
+                    ReleaseDate = DateTime.Parse("2002-4-15"),
+                    Genre = "Romance",
+                    Rating = "PG-13",
+                    Price = 3.99M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
